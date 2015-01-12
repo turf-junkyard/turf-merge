f627e9b4e6dfad9b5531fc4ee4371e6b2569aab9
Tom MacWright <tom@macwright.org>
2015-01-12 18:54:23 -0500

    JSDoc.

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var clone = require('clone');
 var union = require('turf-union');
 
+/**
+ * Takes a feature collection of polygons and outputs a single merged
+ * polygon feature.
+ * @module turf/merge
+ * @param {FeatureCollection} features
+ * @return {Feature}
+ * @example
+ * var a = turf.polygon([[[10,0],[20,10],[20,0],[10,0]]]);
+ * a.properties.fill = '#0f0';
+ * var b = turf.polygon([[[10+5,0+5],[20+5,10+5],[20+5,0+5],[10+5,0+5]]]);
+ * b.properties.fill = '#00f';
+ * var erased = turf.erase(JSON.parse(JSON.stringify(a)), b);
+ * var a = turf.polygon([[[10,0],[20,10],[20,0],[10,0]]]);
+ * a.properties.fill = '#0f0';
+ * var b = turf.polygon([[[10+5,0+5],[20+5,10+5],[20+5,0+5],[10+5,0+5]]]);
+ * b.properties.fill = '#00f';
+ * var merged = turf.merge(turf.featurecollection([a, b]));
+ * //=a
+ * //=b
+ * //=merged
+ */
 module.exports = function(polygons, done){
 
   var merged = clone(polygons.features[0]),
@@ -15,4 +36,4 @@ module.exports = function(polygons, done){
   }
 
   return merged;
-}
+};
